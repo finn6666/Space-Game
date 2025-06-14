25603b26de50c8663d95792478fad4daf38e4d8a
finn6666 <finn81@hotmail.co.uk>
2025-06-14 11:03:11 +0100

    Added modules and starting init

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame 
+from constants import *
+
+
+pygame.init()
+
+
+def main():
+    print("Starting Asteroids!")
+
+def WidthHeight():
+    print(f'Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}')
+
+
+WidthHeight()
+
+
+if __name__ == "__main__":
+    main()
