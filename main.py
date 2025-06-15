ffbe9204ebadb8cabb169dbee322ebabfd3f4c42
finn6666 <finn81@hotmail.co.uk>
2025-06-15 11:20:05 +0100

    Changing from venv to conda

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 import pygame 
 from constants import *
+from player import *
 
 
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+clock = pygame.time.Clock()
+
 def main():
     print("Starting Asteroids!")
-    while 1:
+    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)  # instantiate first
+    dt = 0
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill('black')
+        player.update(dt)
+        player.draw(screen)  # draw player after clearing screen
         pygame.display.flip()
+
+        res = clock.tick(60)
+        dt = res / 1000
 
 def WidthHeight():
     print(f'Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}')
